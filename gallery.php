bfd99663f71b27a62469f00125df0d95576c3f84
dmitrichyonok <dmitrichyonok@inbox.lv>
2016-01-11 17:15:39 +0200

    changes from www
    www

---
+++ gallery.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="lv">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon2.ico">
+
+    <title>Arturs Dmitričonoks</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dist/css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="lightbox2/dist/css/lightbox.min.css">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Home</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="CV.php">CV</a></li>
+            <li><a href="forma.php">Forma</a></li>
+            <li class="active"><a href="gallery.php">Galerija</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div>
+        <a href="lightbox2/img/m4.jpg" data-lightbox="bmw" data-title="BMW M4"><img class="image" src="lightbox2/img/thumb-M4.jpg" alt="Image #1" /></a>
+        <a href="lightbox2/img/E39M5.jpg" data-lightbox="bmw" data-title="BMW M5"><img class="image" src="lightbox2/img/thumb-E39M5.jpg" alt="E39M5" /></a>
+        <a href="lightbox2/img/krisa.jpg" data-lightbox="bmw" data-title="krisa"><img class="image" src="lightbox2/img/thumb-krisa.jpg" alt="krisa" /></a>
+      </div>
+
+    </div><!-- /.container -->
+
+    <footer class="footer">
+      <div class="container">
+      <p class="text-muted">Autors : <!-- Trigger the modal with a button -->
+<button type="button" class="" data-toggle="modal" data-target="#myModal">Arturs Dmitričonoks</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Kontakt informācija:</h4>
+      </div>
+      <div class="modal-body">
+        <p> <a href="mailto:dmitrichyonok@inbox.lv">
+    dmitrichyonok@inbox.lv</a></p>
+    <p>Tālrunis : 27035150</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>  
+    </p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!--  <script scr="dist/js/jquery-1.11.3.min.js"></script> -->
+  <script scr="disc/js/script.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    
+        
+    <!--<script src="lightbox2/dist/js/lightbox-plus-jquery.min.js"></script>-->
+    <script src="lightbox2/dist/js/lightbox.js"></script>
+
+  </body>
+
+</html>
